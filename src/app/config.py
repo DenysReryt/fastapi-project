0c9e393add9ffd5d9693bb247ad051d7bd447f46
Denys <denysreryt28@gmail.com>
2022-10-14 01:01:42 +0300

    docker

---
+++ src/app/config.py
@@ -0,0 +1,8 @@
+import os
+
+# Redis
+REDIS_URL = os.getenv("REDIS_URL")
+
+# Postgres
+POSTGRES_URL = os.getenv("POSTGRES_URL")
+
--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,41 @@
+import databases
+import aioredis
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from config import REDIS_URL, POSTGRES_URL
+
+postgres_db = databases.Database(POSTGRES_URL)
 
 app = FastAPI()
+
+origins = [
+    "http://0.0.0.0:8080",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
 def home():
     return {'status': 'Working'}
+
+
+@app.on_event('startup')
+async def startup():
+    await postgres_db.connect()
+    app.state.redis = await aioredis.from_url(REDIS_URL)
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await postgres_db.disconnect()
+    await app.state.redis.close()
