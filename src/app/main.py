91107ee7e4b6b193b58a8724b410119bc8e6dca2
Denys <denysreryt28@gmail.com>
2022-10-15 23:19:32 +0300

    first commit

--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,18 @@
 import aioredis
+from app.config import REDIS_URL, DATABASE_URL
 
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.models import engine, metadata, database
-from app.config import REDIS_URL
+from fastapi_sqlalchemy import DBSessionMiddleware, db
 
-metadata.create_all(engine)
+from app.schema import User as SchemaUser
+from app.model import User as ModelUser
 
 app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=DATABASE_URL)
 
 origins = [
     "http://0.0.0.0:8080",
@@ -29,19 +32,37 @@ app.add_middleware(
 def status():
     return {"status": "Working"}
 
+@app.post('/user/', response_model=SchemaUser)
+async def user(user: SchemaUser):
+    db_user = ModelUser(
+        first_name = user.first_name,
+        last_name = user.last_name,
+        position = user.position,
+        email = user.email)
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-    app.state.redis = await aioredis.from_url(REDIS_URL)
+    db.session.add(db_user)
+    db.session.commit()
+
+    return db_user
+
+@app.get('/user/')
+async def user():
+    user = db.session.query(ModelUser).all()
+    return user
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-    await app.state.redis.close()
+# @app.on_event("startup")
+# async def startup():
+#     await database.connect()
+#     app.state.redis = await aioredis.from_url(REDIS_URL)
+#
+#
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()
+#     await app.state.redis.close()
 
 
-# to check local
+# to run locally
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
