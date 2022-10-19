a5dd27f7f3f2b417b32e953231b786b537a0a34f
Denys <denysreryt28@gmail.com>
2022-10-19 12:39:51 +0300

    remake3.0

--- src/app/config.py
+++ src/app/config.py
@@ -4,4 +4,4 @@ import os
 REDIS_URL = os.getenv("REDIS_URL", "redis://")
 
 #Postgres
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@d/postgres")
