411716757492d7fb1f67da142e66ebbbb889415d
Denys <denysreryt28@gmail.com>
2022-10-26 16:06:56 +0300

    first commit

--- src/app/user_crud.py
+++ src/app/user_crud.py
@@ -8,24 +8,31 @@ from src.app.schemas import UserBaseSchema
 
 class UserCrud():
 
-    async def get_users(skip: int = 0, limit: int = 100) -> dict:
+    async def create_user_by_email(self, email: str) -> UserBaseSchema:
+        db_user = users.insert().values(first_name='first_name', last_name="last_name", email=email,
+                                        password='password', role='user', verified=False)
+        user_id = await database.execute(db_user)
+        user = await database.fetch_one(users.select().where(users.c.email == email))
+        return schemas.UserBaseSchema(**user.dict(), id=user_id, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+
+    async def get_users(self, skip: int = 0, limit: int = 100) -> UserBaseSchema:
         query = users.select().offset(skip).limit(limit)
         return await database.fetch_all(query=query)
 
-    async def create_user(user: schemas.SignUpSchema) -> UserBaseSchema:
+    async def create_user(self, user: schemas.SignUpSchema) -> UserBaseSchema:
         db_user = users.insert().values(first_name=user.first_name, last_name=user.last_name, email=user.email,
                                         password=user.password, role=user.role, verified=user.verified)
         user_id = await database.execute(db_user)
         return schemas.UserBaseSchema(**user.dict(), id=user_id, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
 
-    async def get_user_by_email(email: str) -> dict:
+    async def get_user_by_email(self, email: str) -> UserBaseSchema:
         return await database.fetch_one(users.select().where(users.c.email == email))
 
-    async def get_user_by_id(id: int) -> dict:
+    async def get_user_by_id(self, id: int) -> UserBaseSchema:
         query = users.select().where(id == users.c.id)
         return await database.fetch_one(query=query)
 
-    async def update_user(id: int, user: schemas.UpdateUserSchema) -> UserBaseSchema:
+    async def update_user(self, id: int, user: schemas.UpdateUserSchema) -> UserBaseSchema:
         query = (users.update().where(id == users.c.id).values(
             first_name=user.first_name,
             last_name=user.last_name,
@@ -36,7 +43,8 @@ class UserCrud():
         user_get_id = await database.fetch_one(users.select().where(id == users.c.id))
         return schemas.UserBaseSchema(**user.dict(), id=user_id, email=user_get_id.email, created_at=user_get_id.created_at, updated_at=user_get_id.updated_at)
 
-    async def delete(id: int) -> dict:
+    async def delete(self, id: int) -> None:
         query = users.delete().where(id == users.c.id)
         return await database.execute(query=query)
 
+crud = UserCrud()
